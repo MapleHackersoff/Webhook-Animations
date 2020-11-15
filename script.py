44b1c10e5226ebae46c80fe55b3b747ad80af705
Kieronia <70661699+kieronia@users.noreply.github.com>
2020-11-15 09:38:12 +0000

    Update script.py
    Make sure to add your webhook!

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import requests,time
 order = "1"
-link1 = "https://ptb.discord.com/api/webhooks/777463973771477012/AD7EDmvcWUszQ7-_sTTuKraglIXYa_Bvh1mxqoV6iLmnnPD5y82qc6RR_kyv6oIAjYwn"
-link2 = "https://ptb.discord.com/api/webhooks/777464001089110026/4IdA-i-A6WAFoQ6IxZDGX4HUB5PGC9Ja5XYv_ag9tvBK8OlI6W7L5OI4z9lexdvsrZRW" #you need 2 to seperate it otherwise itll show the previous pfp
+link1 = "https://ptb.discord.com/api/webhooks/777/dwakjADWDdwkadjadfwhoa" #make 2 webhooks and copy the link
+link2 = "https://ptb.discord.com/api/webhooks/777/wdawaddaWEaDW" #you need 2 to seperate it otherwise itll show the previous pfp
 message = "Demonstration from **https://github.com/kieronia/Webhook-Animations**" #honestly  - while ik this is simple please , if your gonna change it, don't claim you made this, I've put so many hours into my work and It breaks my heart to see it get around 5 stars, copied and pasted and sold etc
 username = "OooooOooooOooo"
 while True:
@@ -19,9 +19,6 @@ while True:
             elif order == "2":
                 webhook = link2 
                 order = "1"
-
-
-
 
             e = requests.post(webhook, data=data).status_code
             if e != 204:
